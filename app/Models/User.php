a7ce0be102caf5a5d737b651787d72fd28b739c0
Riya Roy <riyaroy7222@gmail.com>
2026-01-20 00:27:05 +0530

    Bug Fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'email_verifiactaion_token'
+        'email_verification_token'
     ];
 
     /**
